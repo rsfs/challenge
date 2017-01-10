8a3923ba4baae8e3a4e6a7786bb600de5df22ca9
cielo <cielo@STFALXC037231-W.stefanini.dom>
2017-01-10 11:37:10 -0200

    Removing H2 console configuration

--- src/main/java/com/challenge/Application.java
+++ src/main/java/com/challenge/Application.java
@@ -1,25 +1,15 @@
 package com.challenge;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-    
-    @Bean
-    public ServletRegistrationBean h2servletRegistration() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-        registration.addUrlMappings("/console/*");
-        return registration;
     }
     
     @Override
